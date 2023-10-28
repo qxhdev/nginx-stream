f28c3f28274f1fd3742e97a26a4a25b4f6520719
henry <swift@gmail.com>
2023-10-28 09:26:26 +0800

    init

---
+++ start.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+BASE_DIR=$HOME/data/nginx-stream
+
+if [ ! -d "$BASE_DIR" ];then
+  echo '创建gitlab文件夹'
+  # 创建文件夹
+  mkdir -p "$BASE_DIR"/script
+  mkdir -p "$BASE_DIR"/logs
+fi
+
+docker stop nginx-stream || echo 'stop nginx-stream'
+docker rm nginx-stream || echo 'rm nginx-stream'
+
+
+sudo docker run --name nginx-stream -d --restart always \
+  -p 180:80 -p 1935:1935 \
+  -v "$BASE_DIR"/script:/script \
+  -v "$BASE_DIR"/logs:/var/log/nginx/ \
+  -v /etc/hosts:/etc/hosts \
+  nginx-stream
